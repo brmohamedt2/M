0a63473cbc34da91a44798f061c2b97998a9d9dc
MOHAMED-404 <146913683+brmohamedt2@users.noreply.github.com>
2023-10-04 18:10:17 +0100

    Add files via upload

--- Mohamed.py
+++ Mohamed.py
@@ -1462,7 +1462,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("rhttps://github.com/brmohamedt2/trick.txt/blob/main/trick.txt").text
+	r1=requests.get(''rhttps://github.com/brmohamedt2/trick.txt/blob/main/trick.txt'').text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
